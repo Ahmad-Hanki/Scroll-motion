d61913a7e582471d3209e2fb31cf7a9739c7e0cf
ahmad <itxti909@gmail.com>
2024-05-12 23:10:05 +0300

    parallax

---
+++ src/app/parallax/page.tsx
@@ -0,0 +1,36 @@
+import Parallax from "@/components/Parallax"
+
+const page = () => {
+  return (
+    <section >
+        <Parallax />
+        <div className="w-full bg-black">
+            <div className="max-w-xl space-y-4 mx-auto">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita tempora dolores nostrum nesciunt? Tenetur quaerat praesentium a rerum eveniet dolores consectetur accusantium ipsam nobis aliquam, explicabo quidem neque hic.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default page
